9143004e1785380cc8fa6e21b2a24e4ccf1e6d89
Welheminah <ponelehutso@gmail.com>
2021-04-06 16:02:13 +0200

    to be reviewed

--- mochatest/regChecks.test.js
+++ mochatest/regChecks.test.js
@@ -6,7 +6,7 @@ describe('The regCheck function', function(){
     it('should retrun false if registration dont endswith GP', function(){
         assert.equal(false, regCheck('CY 189-012', 'CA'));
     });
-    it('should retrun false, please enter valid registration number and location, if the string is empty', function(){
+    it('should retrun please enter valid registration number and location, if the string is empty', function(){
         assert.equal('please enter valid registration number and location', regCheck('', ''));
     });
 });
