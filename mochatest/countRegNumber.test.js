ab0dd1cbb0609aa359bf419b7e885141b507132b
Welheminah <ponelehutso@gmail.com>
2021-04-06 15:13:22 +0200

    reg

--- mochatest/countRegNumber.test.js
+++ mochatest/countRegNumber.test.js
@@ -4,7 +4,7 @@ describe('The countRegNumber function', function(){
         assert.equal(countRegNumber('CA 182736,CY 523519,CJ 812328'), 3);
     });
     it('should return length of the registration', function(){
-        assert.equal(countRegNumber('CA 182736,CY 523519,CJ 812328','CA 42665, AA 12 RT GP'), 3, 2);
+        assert.equal(countRegNumber('CA 182736,CY 523519,CJ 812328','CA 42665, CA 12 RT GP'), 3, 2);
     });
     it('should return length of the registration', function(){
         assert.equal(countRegNumber('CA 42665, AA 12 RT GP'), 2);
